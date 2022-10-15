97e59a8efe2ff4fa3054a6a07cfa6ded94afff07
JoseDanielRF <zzjdanielzz@hotmail.com>
2022-10-15 11:15:36 -0500

    bd

--- base-de-datos/trading.sql
+++ base-de-datos/trading.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 12-10-2022 a las 18:34:22
+-- Tiempo de generación: 15-10-2022 a las 18:14:57
 -- Versión del servidor: 10.4.20-MariaDB
 -- Versión de PHP: 8.0.9
 
@@ -68,7 +68,7 @@ CREATE TABLE `comprobantes` (
 --
 
 INSERT INTO `comprobantes` (`id`, `codigo`, `foto`, `fecha`, `estado`, `valor`, `doc_usuario`, `campaña`) VALUES
-(3, 'POS-23FG', '', '2022-10-12', 0, 8000000, 123, 1);
+(3, 'POS-23FG', '', '2022-10-12', 1, 8000000, 1, 1);
 
 -- --------------------------------------------------------
 
@@ -84,13 +84,6 @@ CREATE TABLE `cuentas_bancarias` (
   `tipo` text NOT NULL,
   `fecha` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Volcado de datos para la tabla `cuentas_bancarias`
---
-
-INSERT INTO `cuentas_bancarias` (`numero`, `titular`, `entidad`, `estado`, `tipo`, `fecha`) VALUES
-(99887876767, 123, 'Davivienda', 1, 'ahorros', '2022-10-12 14:58:57');
 
 -- --------------------------------------------------------
 
@@ -151,12 +144,25 @@ CREATE TABLE `red_binaria` (
   `usuario_red` int(11) NOT NULL,
   `orden_binaria` int(11) NOT NULL,
   `derrame_binaria` int(11) NOT NULL,
-  `posicion_binaria` varchar(1) DEFAULT NULL,
   `patrocinador_red` text DEFAULT NULL,
   `periodo_comision` float NOT NULL,
   `periodo_venta` float NOT NULL,
   `fecha_binaria` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `red_binaria`
+--
+
+INSERT INTO `red_binaria` (`id_binaria`, `usuario_red`, `orden_binaria`, `derrame_binaria`, `patrocinador_red`, `periodo_comision`, `periodo_venta`, `fecha_binaria`) VALUES
+(1, 1, 1, 0, 'admin-trading', 0, 0, '2022-10-14 16:58:45'),
+(14, 25, 2, 1, 'admin-trading', 0, 0, '2022-10-15 00:00:26'),
+(15, 26, 3, 1, 'admin-trading', 0, 0, '2022-10-15 00:31:49'),
+(16, 27, 4, 3, 'mateo-26', 0, 0, '2022-10-15 00:34:47'),
+(17, 28, 5, 2, 'juan-25', 0, 0, '2022-10-15 01:05:17'),
+(18, 29, 6, 1, 'admin-trading', 0, 0, '2022-10-15 14:54:55'),
+(19, 30, 7, 3, 'mateo-26', 0, 0, '2022-10-15 15:51:17'),
+(20, 31, 8, 7, 'julian-30', 0, 0, '2022-10-15 16:06:20');
 
 -- --------------------------------------------------------
 
@@ -196,12 +202,14 @@ CREATE TABLE `red_uninivel` (
 --
 
 INSERT INTO `red_uninivel` (`id_uninivel`, `usuario_red`, `patrocinador_red`, `periodo_comision`, `periodo_venta`, `fecha_uninivel`) VALUES
-(6, 7, 'admin-trading', 0, 10, '2022-10-06 23:25:31'),
-(7, 14, 'admin-trading', 0, 10, '2022-10-12 00:29:05'),
-(8, 15, 'prueba-14', 0, 10, '2022-10-12 00:32:00'),
-(9, 16, 'prueba-14', 0, 10, '2022-10-12 15:48:18'),
-(10, 16, 'prueba-14', 0, 10, '2022-10-12 16:14:32'),
-(11, 17, 'prueba-14', 0, 10, '2022-10-12 16:19:40');
+(1, 1, 'admin-trading', 0, 10, '2022-10-14 23:51:17'),
+(21, 25, 'admin-trading', 0, 10, '2022-10-15 00:00:26'),
+(22, 26, 'admin-trading', 0, 10, '2022-10-15 00:31:49'),
+(23, 27, 'mateo-26', 0, 10, '2022-10-15 00:34:47'),
+(24, 28, 'juan-25', 0, 10, '2022-10-15 01:05:17'),
+(25, 29, 'admin-trading', 0, 10, '2022-10-15 14:54:55'),
+(26, 30, 'mateo-26', 0, 10, '2022-10-15 15:51:17'),
+(27, 31, 'julian-30', 0, 10, '2022-10-15 16:06:20');
 
 -- --------------------------------------------------------
 
@@ -233,6 +241,13 @@ CREATE TABLE `soporte` (
   `papelera` text DEFAULT NULL,
   `fecha_soporte` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `soporte`
+--
+
+INSERT INTO `soporte` (`id_soporte`, `remitente`, `receptor`, `asunto`, `mensaje`, `adjuntos`, `tipo`, `papelera`, `fecha_soporte`) VALUES
+(1, 1, 30, 'Obtenga nuestros beneficos', '<p>Obtenga todos nuestros beneficios, no olvide ir a perfil y firmar el contrato.</p>', '[\"vistas\\/img\\/tickets\\/1\\/318.png\"]', 'enviado', NULL, '2022-10-15 15:35:32');
 
 -- --------------------------------------------------------
 
@@ -269,12 +284,14 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`id_usuario`, `doc_usuario`, `perfil`, `nombre`, `email`, `password`, `estado`, `operando`, `ciclo_pago`, `vencimiento`, `verificacion`, `email_encriptado`, `foto`, `enlace_afiliado`, `patrocinador`, `pais`, `codigo_pais`, `telefono_movil`, `firma`, `fecha_contrato`, `fecha`) VALUES
-(18, 0, 'usuario', 'julian', 'julian@gmail.com', '$2a$07$asxx54ahjppf45sd87a5auGZEtGHuyZwm.Ur.FJvWLCql3nmsMbXy', 1, 0, NULL, NULL, 1, '4efe518f863e162e8f00e7d823b40230', NULL, NULL, 'prueba-14', NULL, NULL, NULL, NULL, NULL, '2022-10-12 16:20:25'),
 (1, 1, 'admin', 'Administrador', 'admin@trading.com', '$2a$07$asxx54ahjppf45sd87a5auGZEtGHuyZwm.Ur.FJvWLCql3nmsMbXy', 1, 1, NULL, '2019-10-07', 1, NULL, 'vistas/img/usuarios/1/434.jpg', 'admin-trading', NULL, NULL, NULL, '(573) 218-5749', 'firma', NULL, '2019-09-27 19:13:02'),
-(17, 9, 'usuario', 'mateo', 'mateo@gmail.com', '$2a$07$asxx54ahjppf45sd87a5auGZEtGHuyZwm.Ur.FJvWLCql3nmsMbXy', 1, 0, NULL, NULL, 1, 'e64b49457900a3435c49e2e8ee79f603', NULL, 'mateo-17', 'prueba-14', 'Venezuela', ' Bolivarian Republic of', 'VE (234) 234-2342', '<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?><!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.1//EN\" \"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\"><svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" width=\"123\" height=\"4\"><path stroke-linejoin=\"round\" stroke-linecap=\"round\" stroke-width=\"1\" stroke=\"#333\" fill=\"none\" d=\"M 122 1 c -0.19 0.04 -7.31 1.94 -11 2 c -36.29 0.6 -110 0 -110 0\"/></svg>', '2022-10-12', '2022-10-12 16:14:59'),
-(16, 58, 'usuario', 'Juan', 'juan@gmail.com', '$2a$07$asxx54ahjppf45sd87a5auGZEtGHuyZwm.Ur.FJvWLCql3nmsMbXy', 1, 1, NULL, NULL, 1, '7038663cc684aa330956752c7e6fe7d4', NULL, 'juan-16', 'prueba-14', 'Peru', 'PE', '+51 (123) 123-1231', '<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?><!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.1//EN\" \"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\"><svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" width=\"272\" height=\"6\"><path stroke-linejoin=\"round\" stroke-linecap=\"round\" stroke-width=\"1\" stroke=\"#333\" fill=\"none\" d=\"M 1 5 c 4.34 0 164.44 0.63 248 0 c 5.77 -0.04 11.6 -1.02 17 -2 c 1.7 -0.31 5 -2 5 -2\"/></svg>', '2022-10-12', '2022-10-12 15:43:44'),
-(14, 123, 'usuario', 'prueba', 'prueba@gmail.com', '$2a$07$asxx54ahjppf45sd87a5auGZEtGHuyZwm.Ur.FJvWLCql3nmsMbXy', 1, 1, NULL, NULL, 1, 'c81b5136bcd10b4390108c979ed28ee6', NULL, 'prueba-14', 'admin-trading', 'Colombia', 'CO', '+57 (123) 123-1231', '<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?><!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.1//EN\" \"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\"><svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" width=\"280\" height=\"75\"><path stroke-linejoin=\"round\" stroke-linecap=\"round\" stroke-width=\"1\" stroke=\"#333\" fill=\"none\" d=\"M 52 25 c -0.12 0.07 -4.91 2.4 -7 4 c -3.49 2.67 -6.42 6.25 -10 9 c -5.16 3.97 -10.4 7.71 -16 11 c -5.8 3.41 -17.68 8.85 -18 9 c -0.09 0.04 3.24 -2.23 5 -3 c 3.52 -1.54 7.18 -2.83 11 -4 c 8.48 -2.59 16.57 -5.57 25 -7 c 11.08 -1.88 22.65 -2.54 34 -3 c 5 -0.2 10.09 0.18 15 1 c 7.06 1.18 14.64 2.88 21 5 c 2.12 0.71 4.32 2.5 6 4 c 1.17 1.04 2 2.6 3 4 c 0.72 1.01 1.65 1.96 2 3 c 0.58 1.75 1.17 4.31 1 6 c -0.12 1.25 -1.03 3.22 -2 4 c -1.94 1.56 -5.35 3.71 -8 4 c -8.6 0.93 -19.39 0.47 -29 0 c -4.03 -0.2 -8.24 -0.78 -12 -2 c -7.35 -2.38 -15.31 -5.25 -22 -9 c -6.64 -3.72 -14.08 -8.74 -19 -14 c -4.11 -4.39 -7.75 -11.49 -10 -17 c -1.14 -2.81 -1 -6.72 -1 -10 c 0 -2.96 0.26 -6.2 1 -9 c 0.88 -3.33 1.66 -9.53 4 -10 c 12.16 -2.43 43.15 -3.66 56 -2 c 2.67 0.35 4.35 6.54 6 10 c 1.65 3.47 2.83 7.17 4 11 c 3.97 12.96 6.87 25.62 11 38 c 1.17 3.5 2.98 7.43 5 10 c 1.3 1.66 3.98 3.62 6 4 c 4.19 0.8 10.02 0.36 15 0 c 4.37 -0.31 8.96 -0.63 13 -2 c 12.25 -4.17 24.71 -9.17 37 -15 c 27.26 -12.92 54.11 -28.4 79 -40 c 2.55 -1.19 6.66 -1.78 9 -1 l 12 8\"/></svg>', '2022-10-12', '2022-10-12 00:28:17'),
-(15, 321, 'usuario', 'pedro pablo', 'pedro@gmail.com', '$2a$07$asxx54ahjppf45sd87a5auGZEtGHuyZwm.Ur.FJvWLCql3nmsMbXy', 1, 1, NULL, NULL, 1, 'c3b7f393410fe6185ba5d966a213a38f', NULL, 'pedro-pablo-15', 'prueba-14', 'Chile', 'CL', '+56 (213) 123-1231', '<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?><!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.1//EN\" \"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\"><svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" width=\"165\" height=\"43\"><path stroke-linejoin=\"round\" stroke-linecap=\"round\" stroke-width=\"1\" stroke=\"#333\" fill=\"none\" d=\"M 3 1 c -0.03 0.12 -2.23 4.97 -2 7 c 0.36 3.26 1.88 8.61 4 11 c 2.52 2.83 7.81 5.15 12 7 c 7.07 3.12 14.48 6.15 22 8 c 12.73 3.13 25.82 5.24 39 7 c 6.99 0.93 13.95 1 21 1 c 4.04 0 8.53 -0.31 12 -1 c 1.04 -0.21 2.22 -1.22 3 -2 c 0.78 -0.78 2.33 -2.67 2 -3 c -0.55 -0.55 -6 -1.11 -6 -1 c 0 0.11 4.08 1.89 6 2 c 3.35 0.2 7.41 -0.31 11 -1 c 5.05 -0.97 10 -2.39 15 -4 c 4.48 -1.44 8.76 -3.07 13 -5 l 9 -5\"/></svg>', '2022-10-12', '2022-10-12 00:30:50');
+(28, 43, 'usuario', 'prueba', 'prueba@gmail.com', '$2a$07$asxx54ahjppf45sd87a5auGZEtGHuyZwm.Ur.FJvWLCql3nmsMbXy', 1, 0, NULL, NULL, 1, 'c81b5136bcd10b4390108c979ed28ee6', NULL, 'prueba-28', 'juan-25', 'Colombia', 'CO', '+57 (123) 123-1231', '<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?><!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.1//EN\" \"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\"><svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" width=\"64\" height=\"72\"><path stroke-linejoin=\"round\" stroke-linecap=\"round\" stroke-width=\"1\" stroke=\"#333\" fill=\"none\" d=\"M 1 71 c 0.12 -0.05 4.77 -1.78 7 -3 c 4.46 -2.45 9.26 -4.88 13 -8 c 3.99 -3.33 8.19 -7.78 11 -12 c 2.24 -3.37 2.86 -8.54 5 -12 c 1.98 -3.2 5.72 -5.73 8 -9 c 5.36 -7.69 10.12 -16.96 15 -24 l 3 -2\"/></svg>', '2022-10-06', '2022-10-15 01:04:21'),
+(25, 369, 'usuario', 'Juan', 'juan@gmail.com', '$2a$07$asxx54ahjppf45sd87a5auGZEtGHuyZwm.Ur.FJvWLCql3nmsMbXy', 1, 1, NULL, NULL, 1, '7038663cc684aa330956752c7e6fe7d4', NULL, 'juan-25', 'admin-trading', 'Colombia', 'CO', '+57 (123) 123-2323', '<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?><!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.1//EN\" \"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\"><svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" width=\"222\" height=\"79\"><path stroke-linejoin=\"round\" stroke-linecap=\"round\" stroke-width=\"1\" stroke=\"#333\" fill=\"none\" d=\"M 12 10 c -0.12 0.19 -4.9 7.19 -7 11 c -1.58 2.88 -3.53 6.17 -4 9 c -0.43 2.6 0.13 6.55 1 9 c 0.62 1.73 2.44 4.04 4 5 c 2.31 1.42 5.99 2.5 9 3 c 4.77 0.79 9.92 1 15 1 c 7.78 0 15.7 0.27 23 -1 c 7.63 -1.33 15.92 -3.89 23 -7 c 6.22 -2.73 13.06 -6.79 18 -11 c 3.51 -2.99 7.4 -7.92 9 -12 c 1.72 -4.38 3.11 -14.05 2 -16 c -0.7 -1.23 -7.47 0.36 -10 2 c -8.64 5.61 -19.2 14.2 -27 22 c -4.04 4.04 -7.32 9.8 -10 15 c -2.54 4.93 -4.76 10.85 -6 16 c -0.65 2.71 -1.1 6.91 0 9 c 1.73 3.29 6.5 7.73 10 10 c 2.58 1.68 6.7 2.7 10 3 c 7.27 0.66 15.62 0.99 23 0 c 9.57 -1.29 19.24 -4.19 29 -7 c 10.35 -2.98 20.38 -5.96 30 -10 c 6.94 -2.92 14.96 -6.71 20 -11 c 3.07 -2.61 5.98 -8.25 7 -12 c 0.74 -2.72 -0.25 -6.98 -1 -10 c -0.5 -2.01 -1.83 -4.25 -3 -6 c -0.74 -1.11 -2.11 -3.21 -3 -3 c -4.14 0.99 -13.19 4.73 -18 8 c -2.78 1.89 -4.93 5.89 -7 9 c -1.22 1.83 -2.64 4.22 -3 6 c -0.22 1.1 0.23 3.42 1 4 c 1.36 1.02 4.68 1.89 7 2 c 11.47 0.53 24.36 1.13 36 0 c 8.62 -0.83 17.93 -3.66 26 -6 l 5 -3\"/></svg>', '2022-10-06', '2022-10-14 23:51:52'),
+(27, 765, 'usuario', 'pedro pablo', 'pedro@gmail.com', '$2a$07$asxx54ahjppf45sd87a5auGZEtGHuyZwm.Ur.FJvWLCql3nmsMbXy', 1, 0, NULL, NULL, 1, 'c3b7f393410fe6185ba5d966a213a38f', NULL, 'pedro-pablo-27', 'mateo-26', 'Peru', 'PE', '+51 (123) 123-1231', '<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?><!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.1//EN\" \"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\"><svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" width=\"210\" height=\"20\"><path stroke-linejoin=\"round\" stroke-linecap=\"round\" stroke-width=\"1\" stroke=\"#333\" fill=\"none\" d=\"M 1 1 c 1.58 0 59.27 -0.86 90 0 c 18.15 0.51 35.41 1.32 53 4 c 22 3.36 65 14 65 14\"/></svg>', '2022-10-06', '2022-10-15 00:33:48'),
+(29, 980, 'usuario', 'Luis', 'luis@gmail.com', '$2a$07$asxx54ahjppf45sd87a5auGZEtGHuyZwm.Ur.FJvWLCql3nmsMbXy', 1, 0, NULL, NULL, 1, '19ff31020abd6906f2f975a3e77e07c7', NULL, 'luis-29', 'admin-trading', 'Israel', 'IL', '+972 (787) 767-6676', '<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?><!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.1//EN\" \"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\"><svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" width=\"146\" height=\"54\"><path stroke-linejoin=\"round\" stroke-linecap=\"round\" stroke-width=\"1\" stroke=\"#333\" fill=\"none\" d=\"M 1 53 c 0.07 -0.02 2.96 -0.41 4 -1 c 1.07 -0.61 1.89 -2.26 3 -3 c 1.75 -1.17 3.88 -2.29 6 -3 c 22.86 -7.62 45.47 -15.05 69 -22 c 6.31 -1.86 12.66 -2.27 19 -4 c 8.58 -2.34 16.75 -4.9 25 -8 c 5.21 -1.95 10.83 -4.45 15 -7 l 3 -4\"/></svg>', '2022-10-06', '2022-10-15 14:53:53'),
+(31, 73908, 'usuario', 'Carlos', 'carlos@gmail.com', '$2a$07$asxx54ahjppf45sd87a5auGZEtGHuyZwm.Ur.FJvWLCql3nmsMbXy', 1, 0, NULL, NULL, 1, 'db1e0a3750e0399df3eeee808187d9b4', NULL, 'carlos-31', 'julian-30', 'Chile', 'CL', '+56 (657) 576-5765', '<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?><!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.1//EN\" \"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\"><svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" width=\"165\" height=\"40\"><path stroke-linejoin=\"round\" stroke-linecap=\"round\" stroke-width=\"1\" stroke=\"#333\" fill=\"none\" d=\"M 1 1 c 0.23 0.05 9.43 1.03 13 3 c 8.24 4.56 16.43 13.02 25 18 c 5.37 3.12 11.76 5.07 18 7 c 12.38 3.83 24.99 8.75 37 10 c 12.55 1.3 27.48 0.12 40 -2 c 8.31 -1.41 17.07 -6.62 25 -9 l 5 0\"/></svg>', '2022-10-06', '2022-10-15 16:04:22'),
+(30, 89875, 'usuario', 'Julian', 'julian@gmail.com', '$2a$07$asxx54ahjppf45sd87a5auGZEtGHuyZwm.Ur.FJvWLCql3nmsMbXy', 1, 0, NULL, NULL, 1, '4efe518f863e162e8f00e7d823b40230', NULL, 'julian-30', 'mateo-26', 'Argentina', 'AR', '+54 (875) 858-7587', '<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?><!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.1//EN\" \"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\"><svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" width=\"200\" height=\"90\"><path stroke-linejoin=\"round\" stroke-linecap=\"round\" stroke-width=\"1\" stroke=\"#333\" fill=\"none\" d=\"M 1 74 c 0.04 -0.05 1.19 -2.19 2 -3 c 1.76 -1.76 3.78 -3.78 6 -5 c 10.15 -5.61 21.37 -10.16 32 -16 c 6.68 -3.67 13.53 -7.21 19 -12 c 10.15 -8.89 19.7 -19.85 29 -30 c 1.61 -1.75 4.76 -5.67 4 -6 c -2.11 -0.92 -15.65 -3.18 -20 -1 c -4.96 2.48 -10.22 12.1 -14 18 c -1.21 1.88 -1.74 4.69 -2 7 c -0.39 3.49 -0.69 7.67 0 11 c 0.88 4.21 2.97 9.14 5 13 c 1.14 2.17 3.18 4.18 5 6 c 1.82 1.82 3.92 3.38 6 5 c 0.96 0.75 1.96 1.69 3 2 c 2.03 0.61 4.64 0.95 7 1 c 13.03 0.28 26.52 0.92 39 0 c 4.97 -0.37 10.06 -2.43 15 -4 c 2.42 -0.77 4.84 -1.82 7 -3 c 1.42 -0.77 4.07 -3.05 4 -3 c -0.23 0.15 -9.44 5.62 -13 9 c -2.75 2.61 -5.27 6.53 -7 10 c -1.8 3.6 -2.5 8.24 -4 12 c -0.43 1.07 -1.2 2.46 -2 3 c -0.92 0.61 -3.36 1.57 -4 1 c -1.8 -1.62 -3.76 -6.8 -6 -10 c -2.46 -3.51 -6.16 -6.73 -8 -10 c -0.87 -1.55 -1.98 -5.15 -1 -6 c 4.28 -3.69 15.33 -8.35 23 -13 c 3.54 -2.15 6.53 -4.88 10 -7 c 2.55 -1.56 5.27 -3.44 8 -4 c 7.98 -1.64 17.21 -2.27 26 -3 c 3.36 -0.28 6.81 -0.49 10 0 c 5.26 0.81 11.84 2.25 16 4 l 3 4\"/></svg>', '2022-10-06', '2022-10-15 15:09:33'),
+(26, 879788, 'usuario', 'Mateo', 'mateo@gmail.com', '$2a$07$asxx54ahjppf45sd87a5auGZEtGHuyZwm.Ur.FJvWLCql3nmsMbXy', 1, 0, NULL, NULL, 1, 'e64b49457900a3435c49e2e8ee79f603', NULL, 'mateo-26', 'admin-trading', 'Chile', 'CL', '+56 (123) 123-1231', '<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?><!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.1//EN\" \"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\"><svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" width=\"235\" height=\"68\"><path stroke-linejoin=\"round\" stroke-linecap=\"round\" stroke-width=\"1\" stroke=\"#333\" fill=\"none\" d=\"M 42 48 c 0 -0.1 -0.78 -4.49 0 -6 c 3.01 -5.78 8.11 -13.43 13 -19 c 4.52 -5.14 10.37 -10.01 16 -14 c 4.5 -3.19 12.27 -7.52 15 -8 c 0.86 -0.15 2.39 3.69 2 5 c -1.24 4.12 -4.66 10.77 -8 15 c -5 6.34 -11.59 12.27 -18 18 c -6.45 5.77 -13.01 11.63 -20 16 c -5.99 3.74 -13.29 7.04 -20 9 c -6.49 1.9 -20.56 5.77 -21 3 c -1.14 -7.13 6.56 -41.45 13 -53 c 2.65 -4.76 14.01 -6.45 21 -8 c 7.49 -1.67 16.49 -2.72 24 -2 c 9.01 0.87 18.93 3.76 28 7 c 19.04 6.8 36.91 16.2 56 23 c 10.09 3.6 20.66 6.02 31 8 c 5.18 0.99 11.13 1.72 16 1 c 5.77 -0.85 12.18 -3.49 18 -6 c 8.92 -3.85 26 -13 26 -13\"/></svg>', '2022-10-06', '2022-10-15 00:30:39');
 
 -- --------------------------------------------------------
 
@@ -445,7 +462,7 @@ ALTER TABLE `pagos_uninivel`
 -- AUTO_INCREMENT de la tabla `red_binaria`
 --
 ALTER TABLE `red_binaria`
-  MODIFY `id_binaria` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_binaria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT de la tabla `red_matriz`
@@ -457,7 +474,7 @@ ALTER TABLE `red_matriz`
 -- AUTO_INCREMENT de la tabla `red_uninivel`
 --
 ALTER TABLE `red_uninivel`
-  MODIFY `id_uninivel` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id_uninivel` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
 --
 -- AUTO_INCREMENT de la tabla `referidos`
@@ -469,13 +486,13 @@ ALTER TABLE `referidos`
 -- AUTO_INCREMENT de la tabla `soporte`
 --
 ALTER TABLE `soporte`
-  MODIFY `id_soporte` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_soporte` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id_usuario` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id_usuario` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 
 --
 -- AUTO_INCREMENT de la tabla `videos`
